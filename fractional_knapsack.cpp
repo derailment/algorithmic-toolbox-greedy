88cc1d671d3d8f9d05d1263788f8a89fc6a6587f
Abby <abby8050@gmail.com>
2019-06-03 20:26:07 +0800

    Fractional Knapsack

--- fractional_knapsack.cpp
+++ fractional_knapsack.cpp
@@ -1,13 +1,41 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
 using std::vector;
 
+void selection_sort(vector<double>& b, vector<int>& v, vector<int>& w){
+  for (int i = 0; i < b.size(); i++){
+    int max = i;
+    for (int j = i+1; j < b.size(); j++){
+      if (b[j] > b[max]) {
+        max = j;
+      }
+    }
+    if (max != i) {
+      std::iter_swap(b.begin()+i, b.begin()+max);
+      std::iter_swap(v.begin()+i, v.begin()+max);
+      std::iter_swap(w.begin()+i, w.begin()+max);
+    }
+  }
+}
+
 double get_optimal_value(int capacity, vector<int> weights, vector<int> values) {
   double value = 0.0;
-
-  // write your code here
-
+  int n = weights.size();
+  vector<double> benefits(n);
+  for (int i = 0; i < n; i++) {
+    benefits[i] = (values[i] / (double) weights[i]);
+  }
+  selection_sort(benefits, values, weights);
+  for (int i = 0; i < n; i++) {
+    int used = std::min(capacity, weights[i]);
+    int tryRes = capacity - used;
+    if (tryRes >= 0) {
+      capacity = tryRes;
+      value += benefits[i] * used;
+    }
+  }
   return value;
 }
 
@@ -20,9 +48,7 @@ int main() {
   for (int i = 0; i < n; i++) {
     std::cin >> values[i] >> weights[i];
   }
-
   double optimal_value = get_optimal_value(capacity, weights, values);
-
   std::cout.precision(10);
   std::cout << optimal_value << std::endl;
   return 0;
